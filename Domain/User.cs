a26e859ff2623e04ff6d74319f7c508fda24cf20
tianlei <tian519lei@163.com>
2021-09-11 14:36:11 +0800

    保存提交

---
+++ Domain/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class User
+    {
+        public long ID { get; set; }//ID
+        public string Name { get; set; }//姓名
+        public string Sex { get; set; }//年龄
+        public string BirthDay { get; set; }//出生年月
+        public string SFZH { get; set; }//身份证号
+        public string Phone { get; set; }//电话
+        public string Profession { get; set; }//职业
+        public string Title { get; set; }//职称
+        public string YbCode { get; set; }//医保编码
+        public string WJCode { get; set; }//卫监编码
+
+        public string UserName { get; set; }//用户名
+        public string PassWord { get; set; }//密码
+        public DateTime CreateTime { get; set; }//创建日期
+
+
+
+    }
+}
---
+++ sql/0002-Medical-创建表.sql
@@ -0,0 +1,158 @@
+Use Medical
+go
+
+
+Create Table [User]
+(
+	Id int primary key,
+	Name nvarchar(200),
+	Sex int,
+	BirthDay Date,	
+	SFZH nvarchar(100),
+	Phone nvarchar(100),
+	UserName nvarchar(200),	
+	[Password] nvarchar(100),	
+	Imagepath nvarchar(200),
+	[State] int,
+	CreateTime datetime not null default(getdate()),
+	IsDelete int not null default(0)
+)
+
+begin --Description
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'主键ID' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User', @level2type=N'COLUMN',@level2name=N'Id'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'用户姓名' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User', @level2type=N'COLUMN',@level2name=N'Name'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'性别(国标)1:男2:女' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User', @level2type=N'COLUMN',@level2name=N'Sex'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'出生年月' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User', @level2type=N'COLUMN',@level2name=N'BirthDay'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'身份证' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User', @level2type=N'COLUMN',@level2name=N'SFZH'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'电话' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User', @level2type=N'COLUMN',@level2name=N'Phone'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'用户名' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User', @level2type=N'COLUMN',@level2name=N'UserName'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'密码md5' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User', @level2type=N'COLUMN',@level2name=N'Password'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'状态：0:正常，-1:禁用' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User', @level2type=N'COLUMN',@level2name=N'state'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'头像' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User', @level2type=N'COLUMN',@level2name=N'Imagepath'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'创建日期' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User', @level2type=N'COLUMN',@level2name=N'CreateTime'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'是否删除:0:正常,-1:删除' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User', @level2type=N'COLUMN',@level2name=N'IsDelete'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'用户表' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'User'
+
+end
+
+Create Table Clinic
+(
+	Id int primary key,	
+	Code nvarchar(100),
+	Name nvarchar(200),
+	Category nvarchar(200),
+	Imagepath nvarchar(200),
+	ServiceStart datetime,
+	ServiceEnd datetime,
+	[State] int,
+	CreateTime datetime not null default(getdate()),
+	IsDelete int not null default(0)	
+)
+
+begin --Descrition
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'主键id' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Clinic', @level2type=N'COLUMN',@level2name=N'Id'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'编号' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Clinic', @level2type=N'COLUMN',@level2name=N'Code'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'名称' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Clinic', @level2type=N'COLUMN',@level2name=N'Name'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'种类' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Clinic', @level2type=N'COLUMN',@level2name=N'Category'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'图片路径' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Clinic', @level2type=N'COLUMN',@level2name=N'Imagepath'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'服务开始时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Clinic', @level2type=N'COLUMN',@level2name=N'ServiceStart'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'服务结束时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Clinic', @level2type=N'COLUMN',@level2name=N'ServiceEnd'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'是否删除:0:正常,-1:禁用' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Clinic', @level2type=N'COLUMN',@level2name=N'state'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'创建时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Clinic', @level2type=N'COLUMN',@level2name=N'CreateTime'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'是否删除:0:正常,-1:删除' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Clinic', @level2type=N'COLUMN',@level2name=N'IsDelete'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'机构表' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Clinic'
+
+end
+
+Create Table UserClinic
+(
+	ID int primary key identity(1,1),
+	ClinicID int,
+	UserID int,
+	Boss int,
+	[State] int,
+	CreateTime datetime not null default(getdate()),
+	IsDelete int not null default(0)	
+)
+
+
+begin --Descrition
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'自增主键' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'UserClinic', @level2type=N'COLUMN',@level2name=N'ID'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'机构ID' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'UserClinic', @level2type=N'COLUMN',@level2name=N'ClinicID'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'用户ID' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'UserClinic', @level2type=N'COLUMN',@level2name=N'UserID'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'是否老板:0否,1:是' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'UserClinic', @level2type=N'COLUMN',@level2name=N'Boss'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'是否删除:0:正常,-1:禁用' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'UserClinic', @level2type=N'COLUMN',@level2name=N'state'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'创建时间' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'UserClinic', @level2type=N'COLUMN',@level2name=N'CreateTime'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'是否删除:0:正常,-1:删除' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'UserClinic', @level2type=N'COLUMN',@level2name=N'IsDelete'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'用户机构关联表' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'UserClinic'
+
+end
+
+Create	Table Department
+(
+	ID int primary key,
+	Name nvarchar(200),
+	BureauCode nvarchar(100),
+	Code nvarchar(100),
+	Kind int,
+	Star int,
+	Descrition nvarchar(200),	
+	IsDelete int not null default(0)
+)
+
+begin --Description
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'主键ID' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Department', @level2type=N'COLUMN',@level2name=N'ID'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'科室名称' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Department', @level2type=N'COLUMN',@level2name=N'Name'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'医保编码' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Department', @level2type=N'COLUMN',@level2name=N'BureauCode'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'自定义编码' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Department', @level2type=N'COLUMN',@level2name=N'Code'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'分类' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Department', @level2type=N'COLUMN',@level2name=N'Kind'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'是否常规:0:否,1:是' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Department', @level2type=N'COLUMN',@level2name=N'Star'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'描述' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Department', @level2type=N'COLUMN',@level2name=N'Descrition'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'是否删除:0:正常,-1:删除' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Department', @level2type=N'COLUMN',@level2name=N'IsDelete'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'科室表' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Department'
+
+end
+
+
+
